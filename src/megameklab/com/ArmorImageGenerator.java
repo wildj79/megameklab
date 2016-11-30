3ad18dad68159f887d5496b25ac6c02972e16f8e
Carl Spain <neoancient.bt@gmail.com>
2016-11-30 12:33:07 -0600

    Added skeleton for armor pip svg generator.

---
+++ src/megameklab/com/ArmorImageGenerator.java
@@ -0,0 +1,13 @@
+package megameklab.com;
+
+public class ArmorImageGenerator {
+
+	public static void main(String[] args) {
+		generateVTOLPips();
+	}
+	
+	private static void generateVTOLPips() {
+		
+	}
+
+}
